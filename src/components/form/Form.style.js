04c1cb30d6766e837125c8006317f1e5acefd53e
Victor Ramon <victormanuelchica@gmail.com>
2022-04-08 11:15:37 +0200

    Create form and added styles

---
+++ src/components/form/Form.style.js
@@ -0,0 +1,19 @@
+import styled from "@emotion/styled";
+
+export const FormInputSubmit = styled.input`
+  background-color: #9497ff;
+  border: none;
+  width: 100%;
+  padding: 10px;
+  color: #fff;
+  font-weight: 700;
+  text-transform: uppercase;
+  font-size: 20px;
+  border-radius: 5px;
+  transition: background-color 0.3s ease;
+
+  &:hover {
+    background-color: #7a7dfe;
+    cursor: pointer;
+  }
+`;
