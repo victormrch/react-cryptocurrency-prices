32aaaea64ebba6ed1f4c4d18ab6db5d926639439
Victor Ramon <victormanuelchica@gmail.com>
2022-04-10 21:27:12 +0200

    Add preview script

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { AppContainer, AppHeading, AppImage } from './App.style';
 import CryptoImage from './img/principal-cryptos.png';
-import Form from './components/Form/Form.component';
-import Result from './components/Result/Result.component';
+import Form from './components/form/Form.component';
+import Result from './components/result/Result.component';
 import { useState, useEffect } from 'react';
 import { getCryptoPrices } from './services/cryptocurrenciesPrices';
 
